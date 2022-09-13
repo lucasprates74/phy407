9cf47930de8fe929f9a098fa9f706dfad87b0349
lucasprates74 <79284676+lucasprates74@users.noreply.github.com>
2022-09-13 14:49:47 -0400

    reformated q1 file to run both cases

--- lab1/Q1.py
+++ lab1/Q1.py
@@ -18,35 +18,41 @@ y[0] = y0
 vx[0] = vx0 
 vy[0] = vy0 
 
-#Loop for 10**4 steps and solve equations using Euler-Cromer method
-for i in range(1, N):
-    vx[i] = vx[i-1] + myf.gravity_rel(x[i-1], y[i-1])[0]*dt
-    vy[i] = vy[i-1] + myf.gravity_rel(x[i-1], y[i-1])[1]*dt
-    x[i] = x[i-1] + vx[i]*dt
-    y[i] = y[i-1] + vy[i]*dt
+def main(func, ver):
+    #Loop for 10**4 steps and solve equations using Euler-Cromer method
+    for i in range(1, N):
+        vx[i] = vx[i-1] + func(x[i-1], y[i-1])[0]*dt
+        vy[i] = vy[i-1] + func(x[i-1], y[i-1])[1]*dt
+        x[i] = x[i-1] + vx[i]*dt
+        y[i] = y[i-1] + vy[i]*dt
 
-#Compute angular momentum
-L = myf.MJUP*(x*vy - y*vx) #CHANGE MASS TO MERCURY
+    #Compute angular momentum
+    L = (x*vy - y*vx) #CHANGE MASS TO MERCURY
 
-#Plot position of planet (x, y) space
-time = np.array(range(N))*dt
-plt.plot(x, y)
-plt.xlabel('$x$ (AU)')
-plt.ylabel('$y$ (AU)')
-plt.title('Orbital Position')
-plt.show()
+    #Plot position of planet (x, y) space
+    time = np.array(range(N))*dt
+    plt.plot(x, y)
+    plt.xlabel('$x$ (AU)')
+    plt.ylabel('$y$ (AU)')
+    plt.title('Orbital Position ({})'.format(ver))
+    plt.show()
 
-#Plot velocity components
-plt.plot(time, vx, label='$v_x$')
-plt.plot(time, vy, label='$v_y$')
-plt.legend()
-plt.xlabel('Time $t$ (yr)')
-plt.ylabel('Velocity (m/s)')
-plt.title('Velocity vs Time')
-plt.show()
+    #Plot velocity components
+    plt.plot(time, vx, label='$v_x$')
+    plt.plot(time, vy, label='$v_y$')
+    plt.legend()
+    plt.xlabel('Time $t$ (yr)')
+    plt.ylabel('Velocity (m/s)')
+    plt.title('Velocity vs Time ({})'.format(ver))
+    plt.show()
 
-#Plot angular momentum magnitude
-plt.plot(time, L)
-plt.xlabel('Time $t$ (yr)')
-plt.ylabel('Angular Momentum $||\\overrightarrow{{L}}||$ ()')
-plt.show('Angular Momentum vs time')
+    #Plot angular momentum magnitude
+    plt.plot(time, L)
+    plt.xlabel('Time $t$ (yr)')
+    plt.ylabel('Angular Momentum $||\\overrightarrow{{L}}||$ ()')
+    plt.title('Angular Momentum vs time ({})'.format(ver))
+    plt.show()
+
+if __name__ == '__main__':
+    main(myf.gravity, 'Newtonian')
+    main(myf.gravity_rel, 'Relativistic')
