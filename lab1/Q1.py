6ccaa983d7c9f17a9efcb04ecfaae4ebaf69b37f
Sam <sam.deabreu@mail.utoronto.ca>
2022-09-14 09:00:33 -0400

    Finished Q1

--- lab1/Q1.py
+++ lab1/Q1.py
@@ -1,24 +1,31 @@
-from tkinter import N
+"""
+Q1 code. External function and variable calls to myFunction.py for gravity formulas and constants.
+Authors: Sam De Abreu 
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 import myFunctions as myf
 # initial conditions
 x0, y0 = .47, 0  # AU
 vx0, vy0 = 0, 8.17  # AU / yr
-dt = 10 ** -4 # yr
+dt = 10  ** -4 # yr
 N = int(1/dt) #steps
 
-#Initialize arrays with initial conditions
-x = np.zeros(N)
-y = np.zeros(N)
-vx = np.zeros(N)
-vy = np.zeros(N)
-x[0] = x0
-y[0] = y0
-vx[0] = vx0 
-vy[0] = vy0 
-
-def main(func, ver):
+def main(func, ver): #Main algorithm for computing and plotting orbits
+    """
+    Main algorithm for computing and plotting orbits
+    INPUT: func is the acceleration formula and ver is either Newtonian or Relativistic 
+    OUTPUT: Plots
+    """
+    #Initialize arrays with initial conditions
+    x = np.zeros(N)
+    y = np.zeros(N)
+    vx = np.zeros(N)
+    vy = np.zeros(N)
+    x[0] = x0
+    y[0] = y0
+    vx[0] = vx0 
+    vy[0] = vy0 
     #Loop for 10**4 steps and solve equations using Euler-Cromer method
     for i in range(1, N):
         vx[i] = vx[i-1] + func(x[i-1], y[i-1])[0]*dt
@@ -27,15 +34,18 @@ def main(func, ver):
         y[i] = y[i-1] + vy[i]*dt
 
     #Compute angular momentum
-    L = (x*vy - y*vx) #CHANGE MASS TO MERCURY
+    L = (x*vy - y*vx) 
 
     #Plot position of planet (x, y) space
     time = np.array(range(N))*dt
     plt.plot(x, y)
+    plt.xlim(-0.5, 0.5)
+    plt.ylim(-0.5, 0.5)
     plt.xlabel('$x$ (AU)')
     plt.ylabel('$y$ (AU)')
     plt.title('Orbital Position ({})'.format(ver))
-    plt.show()
+    plt.savefig('position_{0}.png'.format(ver))
+    plt.clf()
 
     #Plot velocity components
     plt.plot(time, vx, label='$v_x$')
@@ -44,15 +54,19 @@ def main(func, ver):
     plt.xlabel('Time $t$ (yr)')
     plt.ylabel('Velocity AU/yr)')
     plt.title('Velocity vs Time ({})'.format(ver))
-    plt.show()
+    plt.savefig('velocity_{0}.png'.format(ver))
+    plt.clf()
 
     #Plot angular momentum magnitude
+    print(L)
     plt.plot(time, L)
+    plt.ylim(2,5)
     plt.xlabel('Time $t$ (yr)')
-    plt.ylabel('Angular Momentum $||\\overrightarrow{{L}}||$ (AU$^2$/yr)')
-    plt.title('Angular Momentum vs time ({})'.format(ver))
-    plt.show()
+    plt.ylabel('Angular Momentum per unit mass $||\\overrightarrow{{L}}||/M_M$ (AU$^2$/yr)')
+    plt.title('Angular Momentum vs Time ({})'.format(ver))
+    plt.savefig('angularmomentum_{0}.png'.format(ver))
+    plt.clf()
 
-if __name__ == '__main__':
-    main(myf.gravity, 'Newtonian')
-    main(myf.gravity_rel, 'Relativistic')
+if __name__ == '__main__': 
+    main(myf.gravity, 'Newtonian') #Newtonian orbits, Q1c
+    main(myf.gravity_rel, 'Relativistic') #Relativistic orbits, Q1d
--- lab1/myFunctions.py
+++ lab1/myFunctions.py
@@ -1,3 +1,7 @@
+"""
+Functions for computing the acceleration of the Earth due to gravitational forces from the Sun.  
+Authors: Lucas Prates 
+"""
 import numpy as np
 
 # constants 
@@ -7,13 +11,25 @@ RJUP = 5.2  # AU
 ALPHA = .01  # AU^2  # alpha larger to see relativistic effects
 
 def gravity(x, y):
-    # returns the acceleration due to gravity in AU/yr^2 as a numpy array
+    """
+    Computes the acceleration due to gravity using Newtonian mechanics in AU/yr^2 as a numpy array
+    INPUT:
+    Position x and y (AU)
+    OUTPUT:
+    numpy array of the form (x acceleration, y acceleration)
+    """
     coords = np.array([x, y])
     r = np.sqrt(x**2 + y**2)
     return -G * coords / r ** 3
 
 def gravity_rel(x, y):
-    # returns the acceleration due to relativistic gravity in AU/yr^2 as a numpy array
+    """
+    Computes the acceleration due to gravity using relativistic mechanics in AU/yr^2 as a numpy array
+    INPUT:
+    Position x and y (AU)
+    OUTPUT:
+    numpy array of the form (x acceleration, y acceleration)
+    """
     coords = np.array([x, y])
     r = np.sqrt(x**2 + y**2)
     return gravity(x, y) - G * ALPHA * coords / r ** 5
