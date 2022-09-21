d969b5ca99dcd872d193d140a078e7ea08b99be8
lucasprates74 <79284676+lucasprates74@users.noreply.github.com>
2022-09-21 10:36:08 -0400

    added part d

--- lab2/Lab02_Q4.py
+++ lab2/Lab02_Q4.py
@@ -11,10 +11,13 @@ def q(x):
     """Algebraically expanded form of (1 - x) ** 8. Absolutely horrendous looking"""
     return 1 - 8 * x + 28 * x ** 2 - 56 * x ** 3 + 70 * x ** 4 - 56 * x ** 5 + 28 * x ** 6 - 8 * x ** 7 + x ** 8
 
+def f(x):
+    return  (x ** 8) / ((x ** 4) * (x ** 4))
+
 if __name__ == '__main__':
 
     # initialize x array
-    start, stop = 0.98, 1
+    start, stop = 0.98, 1.02
     step = (stop - start) / 500
     x=np.arange(start, stop, step)
 
@@ -36,15 +39,21 @@ if __name__ == '__main__':
     plt.hist(diff, bins=30, edgecolor='black')
     plt.show()
 
-    # expected_std = np.std(diff, ddof=1)
-    # sigma = C * np.sqrt(len(diff) * np.mean(diff ** 2))
-    # print('Standard Deviation:', expected_std)
-    # print('Sigma:', sigma)
+    expected_std = np.std(diff, ddof=1)
+    sigma = C * np.sqrt(len(diff) * np.mean(diff ** 2))
+    print('Standard Deviation:', expected_std)
+    print('Sigma:', sigma)
 
 
     # part c
     relative_err = np.abs(pvals - qvals) / np.abs(pvals)
 
-    upper=60
+    upper=30
     plt.plot(x[:upper], relative_err[:upper], linestyle='none', marker='.')
+    plt.show()
+
+    # part d
+    # plt.plot(x, linestyle='none', marker='.', label='constant value 1')
+    plt.plot(x, f(x)-np.ones(len(x)), linestyle='none', marker='.', label='f(x)')
+    plt.legend()
     plt.show()
