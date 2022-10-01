ae5a30a1a7da47d6a6c804864a063e376c4b608e
Sam <sam.deabreu@mail.utoronto.ca>
2022-10-01 16:03:32 -0400

    save figure stuff

--- lab04/Lab04_Q3.py
+++ lab04/Lab04_Q3.py
@@ -5,6 +5,7 @@ Authors: Sam De Abreu & Lucas Prates
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.constants as CON
+plt.rcParams.update({'font.size': 16}) # change plot font size
 
 def relaxation(func, x_i, epsilon, c=1, omega=0):
     """
@@ -45,7 +46,7 @@ def g(x): # Function used in Exercise 6.13
 
 if __name__== '__main__':
     #Exercise 6.10, part a
-    print('Solution to equation (within 10^(-6)) with initial guess x_i = {0} and c = {1}: x = {2}'.format(0.5, 2, relaxation(f, 0.5, 1e-6, c=2)))
+    print('Solution to equation (within 10^(-6)) with initial guess x_i = {0} and c = {1}: x = {2}'.format(0.5, 2, relaxation(f, 0.5, 1e-6, c=2)[0]))
     # part b
     solutions = []
     c_values = np.arange(0, 3.01, 0.01)
@@ -54,8 +55,8 @@ if __name__== '__main__':
     plt.plot(c_values, solutions)
     plt.xlabel('Parameter $c$')
     plt.ylabel('Solution $x$')
-    plt.title('Bifurcation Plot of Equation (something) With Initial Guess $x_i=0.5$')
-    plt.show()
+    plt.title('Bifurcation Plot of $x=1-e^{-cx}$ With Initial Guess $x_i=0.5$')
+    plt.savefig('Q3a.png', dpi=300, bbox_inches='tight')
 
     # Exercise 6.11
     omega = np.arange(0, 1.5, 0.05)
@@ -66,7 +67,7 @@ if __name__== '__main__':
     plt.xlabel('$\\omega$')
     plt.ylabel('Number of iterations')
     plt.title('Number of iterations vs $\\omega$')
-    plt.show()
+    plt.savefig('Q3b.png', dpi=300, bbox_inches='tight')
 
     # Exercise 6.13, part b
     sol = binary_search(g, 0.5, 6, 1e-6)
