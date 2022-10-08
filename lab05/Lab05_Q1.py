b73484ee153426effcd27fed9a89fefa205333be
lucasprates74 <79284676+lucasprates74@users.noreply.github.com>
2022-10-08 11:37:37 -0400

    implemented b

--- lab05/Lab05_Q1.py
+++ lab05/Lab05_Q1.py
@@ -18,12 +18,40 @@ def acceleration(x, v):
     """
     return -(SPRING_CON / MASS) * x * (1 - v ** 2 / c ** 2) ** (3/2)
 
+def normalized_fft(data):
+    amplitudes = np.abs(np.fft.rfft(data))
+    return amplitudes / max(amplitudes)
+
+
 if __name__ == '__main__':
 
     # part a
-    x0, x1, x2 = 1, xc, 10 * xc
+    x0_slow, x0_fast, x0_rel = 1, xc, 10 * xc
+    
+    T = 2 * np.pi / OMEGA  # approximate period for a classical oscillator
+    t_slow, x_slow, v_slow = myf.EulerCromer(x0_slow, 0, acceleration, 10 * T, 10 ** 3)
 
-    t, x, v = myf.EulerCromer(x0, 0, acceleration, 100, 1000)
+    plt.plot(t_slow, x_slow)
+    plt.show()
 
-    plt.plot(t, x)
+    T = 2 * np.pi / OMEGA  # approximate period for an oscillator nearing the speed of light
+    t_fast, x_fast, v_fast = myf.EulerCromer(x0_fast, 0, acceleration, 10 * T, 10 ** 5)
+
+    plt.plot(t_fast, x_fast)
+    plt.show()
+
+    T = 4 * 10 * xc / c  # approximate period for an oscillator nearing the speed of light
+    t_rel, x_rel, v_rel = myf.EulerCromer(x0_rel, 0, acceleration, 10 * T, 10 ** 6)
+
+    plt.plot(t_rel, x_rel)
+    plt.show()
+
+    #  part b
+    amplitudes_slow = normalized_fft(x_slow)[:100]
+    amplitudes_fast = normalized_fft(x_fast)[:100]
+    amplitudes_rel = normalized_fft(x_rel)[:100]
+    plt.plot(amplitudes_slow, label='1m')
+    plt.plot(amplitudes_fast, label='xc')
+    plt.plot(amplitudes_rel, label='10xc')
+    plt.legend()
     plt.show()
