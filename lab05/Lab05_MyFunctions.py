552bda92478a96dc0b17bd7c451b7e79e6817b93
lucasprates74 <79284676+lucasprates74@users.noreply.github.com>
2022-10-08 15:59:09 -0400

    added gaussian integration

--- lab05/Lab05_MyFunctions.py
+++ lab05/Lab05_MyFunctions.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def EulerCromer(x0, v0, func, end_time, num_points):
+def EulerCromer(x0, v0, func, tstop, dt):
     """
     Solves the ode system 
 
@@ -10,8 +10,10 @@ def EulerCromer(x0, v0, func, end_time, num_points):
     given initial conditions x0, v0 for the time range 0 <= t <= end_time using
     the Euler Cromer methond with num_points samples.
     """
-    dt = end_time / num_points
-    t = np.arange(0, end_time, dt)
+
+    t = np.arange(0, tstop, dt)
+    num_points = len(t)
+    print(num_points)
     x, v = np.zeros(num_points), np.zeros(num_points)
 
     # set initial conditions
@@ -22,3 +24,33 @@ def EulerCromer(x0, v0, func, end_time, num_points):
         x[i] = v[i] * dt + x[i - 1]
 
     return t, x, v
+
+from numpy import ones,copy,cos,tan,pi,linspace
+
+def gaussxw(N):
+
+    # Initial approximation to roots of the Legendre polynomial
+    a = linspace(3,4*N-1,N)/(4*N+2)
+    x = cos(pi*a+1/(8*N*N*tan(a)))
+
+    # Find roots using Newton's method
+    epsilon = 1e-15
+    delta = 1.0
+    while delta>epsilon:
+        p0 = ones(N,float)
+        p1 = copy(x)
+        for k in range(1,N):
+            p0,p1 = p1,((2*k+1)*x*p1-k*p0)/(k+1)
+        dp = (N+1)*(p0-x*p1)/(1-x*x)
+        dx = p1/dp
+        x -= dx
+        delta = max(abs(dx))
+
+    # Calculate the weights
+    w = 2*(N+1)*(N+1)/(N*N*(1-x*x)*dp*dp)
+
+    return x,w
+
+def gaussxwab(N,a,b):
+    x,w = gaussxw(N)
+    return 0.5*(b-a)*x+0.5*(b+a),0.5*(b-a)*w
