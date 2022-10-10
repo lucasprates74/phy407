8013bbae2917f0a7c58e15c91b014b187e53e571
Sam <sam.deabreu@mail.utoronto.ca>
2022-10-10 15:31:49 -0400

    Q2 documentation

--- lab05/Lab05_Q2.py
+++ lab05/Lab05_Q2.py
@@ -1,70 +1,54 @@
-""" The scipy.io.wavfile allows you to read and write .wav files """
+"""
+Q2 code. Filters frequencies above a specified cutoff from an audio file using Fourier Transforms, making the most fire mixtapes.
+Authors: Sam De Abreu & Lucas Prates
+"""
 from scipy.io.wavfile import read, write
 import numpy as np
 import matplotlib.pyplot as plt
 import Lab05_MyFunctions as myf
 plt.rcParams.update({'font.size': 16}) # change plot font size
 
-# read the data into two stereo channels
-# sample is the sampling rate, data is the data in each channel,
-1
-# dimensions [2, nsamples]
-sample, data = read('GraviteaTime.wav')
-# sample is the sampling frequency, 44100 Hz
-# separate into channels
-channel_0 = data[:, 0]
-channel_1 = data[:, 1]
-N_Points = len(channel_0)
-# ... do work on the data...
-
-time = np.linspace(0,len(data)//sample, len(data))  # create time array for data
-
-# part b plot two channels
-figj, axj = plt.subplots(2, 1, figsize=(8,8))
-plt.suptitle('GraviteaTime Wave Form')
-axj[0].plot(time, channel_0)
-axj[0].set_ylabel('Channel 0')
-axj[1].plot(time, channel_1)
-axj[1].set_ylabel('Channel 1')
-plt.xlabel('Time (sec)')
-
-# optional part c
-# axj[0].set_xlim(0, 0.02)
-# axj[1].set_xlim(0, 0.02)
-
-plt.tight_layout()
-plt.show()
-
-# part d
+def read_file(name):
+    """
+    Read .wav file and store channel 0 & 1 data into separate variables. Return both channels, original data (for shape) and sample rate (Hz/sample)
+    """
+    sample, data = read(name)
+    channel_0 = data[:, 0]
+    channel_1 = data[:, 1]
+    return channel_0, channel_1, data, sample
 
 def filter_func(freq, amplitudes, cutoff):
+    """Filter function for cutting off frequencies above the specified cutoff of a fourier transform"""
 
-    cutoff_index = np.argwhere(freq < cutoff).ravel()[-1]
+    cutoff_index = np.argwhere(freq < cutoff).ravel()[-1] # Find index where frequency passes cutoff
     
-    filtered = amplitudes.copy()
-    filtered[cutoff_index:] = 0
+    filtered = amplitudes.copy() # Get a deep copy
+    filtered[cutoff_index:] = 0 # Set frequencies above cutoff to 0
     return filtered
 
+def fourier_filter(channel, name, cutoff=880):
+    """
+    Takes in channel and filters the audio such that it removes all audio above the cut off frequency
+    """
+    coefficients = np.fft.rfft(channel) # Perform fourier transform on channel
+    ang_freq = 2 * np.pi * np.arange(len(coefficients)) / 8  # Get the angular frequency for as per the index of the fourier coefficients (Period=8s)
 
-def fourier_filter(channel, name):
-    coefficients = np.fft.rfft(channel)
-    ang_freq = 2 * np.pi * np.arange(len(coefficients)) / 8  
-
-    cutoff = 880 # Hz
-    filtered = filter_func(ang_freq, coefficients, 2 * np.pi *cutoff)
+    filtered = filter_func(ang_freq, coefficients, 2 * np.pi *cutoff) # Filter the fourier transform
     
+    # Plot original Fourier transform of waveform and filtered Fourier transform of waveform 
     figj, axj = plt.subplots(2, 1, figsize=(8,8))
     plt.suptitle('GraviteaTime FFT for Channel {}'.format(name))
-    axj[0].plot(ang_freq, np.abs(filtered))
+    axj[0].plot(ang_freq, np.abs(coefficients))
     axj[0].set_ylabel('Amplitude')
     axj[1].plot(ang_freq, np.abs(filtered))
     axj[1].set_ylabel('Filtered Amplitude')
     plt.xlabel('Angular Frequency (s$^{-1}$)')
     plt.tight_layout()
-    plt.show()
+    plt.savefig('Q2c_Fourier_channel_{0}.png'.format(name), dpi=300, bbox_inches='tight')
 
-    filtered_signal = np.fft.irfft(filtered)
+    filtered_signal = np.fft.irfft(filtered) # Inverse Fourier transform on filtered Fourier transform, to get the final filitered waveform
 
+    # Plot original waveform and filtered waveform
     figj, axj = plt.subplots(2, 1, figsize=(8,8))
     plt.suptitle('GraviteaTime Wave Form for Channel {}'.format(name))
     axj[0].plot(time, channel)
@@ -73,18 +57,43 @@ def fourier_filter(channel, name):
     axj[1].set_ylabel('Filtered Signal')
     axj[0].set_xlim(0, 0.05)
     axj[1].set_xlim(0, 0.05)    
-    plt.xlabel('Time (sec)')
+    plt.xlabel('Time (s)')
     plt.tight_layout()
-    plt.show()
+    plt.savefig('Q2c_Waveform_channel_{0}.png'.format(name), dpi=300, bbox_inches='tight')
     
     return filtered_signal
 
-channel_0_out = fourier_filter(channel_0, 0)
-channel_1_out = fourier_filter(channel_1, 1)
-# this creates an empty array data_out with the same shape as "data"
-# (2 x N_Points) and the same type as "data" (int16)
-data_out = np.empty(data.shape, dtype = data.dtype)
-# fill data_out
-data_out[:, 0] = channel_0_out
-data_out[:, 1] = channel_1_out
-write('Filtered_GraviteaTime.wav', sample, data_out)
+def write_file(data, channel_0_out, channel_1_out, sample):
+    """
+    Write to .wav file
+    """
+    data_out = np.empty(data.shape, dtype = data.dtype)
+    data_out[:, 0] = channel_0_out
+    data_out[:, 1] = channel_1_out
+    write('Filtered_GraviteaTime.wav', sample, data_out)
+
+
+if __name__ == '__main__':
+    # Getting/reading the data and storing it
+    channel_0, channel_1, data, sample = read_file('GraviteaTime.wav')
+    
+    N = len(data)
+    time = np.linspace(0, N//sample, N)  # create time array for data
+    
+    # part b , plotting original wave forms
+    figj, axj = plt.subplots(2, 1, figsize=(8,8))
+    plt.suptitle('GraviteaTime Wave Form')
+    axj[0].plot(time, channel_0)
+    axj[0].set_ylabel('Channel 0 Waveform')
+    axj[1].plot(time, channel_1)
+    axj[1].set_ylabel('Channel 1 Waveform')
+    plt.xlabel('Time (s)')
+    plt.tight_layout()
+    plt.savefig('Q2b.png', dpi=300, bbox_inches='tight')
+
+    # part d
+    channel_0_out = fourier_filter(channel_0, 0)
+    channel_1_out = fourier_filter(channel_1, 1)
+
+    # part e
+    write_file(data, channel_0_out, channel_1_out, sample)
