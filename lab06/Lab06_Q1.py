3f20dc8423b1f33a0c9047fe3e2d7c73c7eef8b0
Sam <sam.deabreu@mail.utoronto.ca>
2022-10-15 14:38:58 -0400

    Q1

--- lab06/Lab06_Q1.py
+++ lab06/Lab06_Q1.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+N = 4000
 # vec{r} = (x1_1, x1_2, )
 def f(r): 
     #x(variable index)_(Nth particle)
@@ -17,13 +17,13 @@ def f(r):
     return np.array([fx1, fy1, fx2, fy2])
 
 def solve(r1, r2, v1, v2, dt=0.01):
-    r = np.zeros((100, 4))
+    r = np.zeros((N, 4))
     r[0][0], r[0][1] = r1
     r[0][2], r[0][3] = r2
-    v = np.zeros((100, 4))
+    v = np.zeros((N, 4))
     v[0][0], v[0][1] = v1
     v[0][2], v[0][3] = v2
-    v_half = np.zeros((100, 4))
+    v_half = np.zeros((N, 4))
     v_half[0] = v[0]+dt/2*f(r[0])
     for i in range(len(r)-1): # dt -> 2dt
         r[i+1] = r[i]+dt*v_half[i]
@@ -35,18 +35,30 @@ def solve(r1, r2, v1, v2, dt=0.01):
 if __name__ == '__main__':
     r1_sol, v1_sol = solve((4, 4), (5.2, 4), (0, 0), (0, 0))
     for i in range(0, len(r1_sol)):
-        plt.plot(r1_sol[i][0], r1_sol[i][1], marker='o', linestyle='None', color='blue')
-        plt.plot(r1_sol[i][2], r1_sol[i][3], marker='o', linestyle='None', color='red')
+        plt.plot(r1_sol[i][0], r1_sol[i][1], marker='.', linestyle='None', color='blue', alpha=i/N)
+        plt.plot(r1_sol[i][2], r1_sol[i][3], marker='.', linestyle='None', color='red', alpha=i/N)
+    plt.show()
+
+    plt.plot(np.arange(0, N, 1), np.transpose(r1_sol)[0], color='blue')
+    plt.plot(np.arange(0, N, 1), np.transpose(r1_sol)[2], color='red')
     plt.show()
 
     r1_sol, v1_sol = solve((4.5, 4), (5.2, 4), (0, 0), (0, 0))
     for i in range(0, len(r1_sol)):
-        plt.plot(r1_sol[i][0], r1_sol[i][1], marker='o', linestyle='None', color='blue')
-        plt.plot(r1_sol[i][2], r1_sol[i][3], marker='o', linestyle='None', color='red')
+        plt.plot(r1_sol[i][0], r1_sol[i][1], marker='.', linestyle='None', color='blue', alpha=i/N)
+        plt.plot(r1_sol[i][2], r1_sol[i][3], marker='.', linestyle='None', color='red', alpha=i/N)
+    plt.show()
+
+    plt.plot(np.arange(0, N, 1), np.transpose(r1_sol)[0], color='blue')
+    plt.plot(np.arange(0, N, 1), np.transpose(r1_sol)[2], color='red')
     plt.show()
 
     r1_sol, v1_sol = solve((2, 3), (3.5, 4.4), (0, 0), (0, 0))
     for i in range(0, len(r1_sol)):
-        plt.plot(r1_sol[i][0], r1_sol[i][1], marker='o', linestyle='None', color='blue')
-        plt.plot(r1_sol[i][2], r1_sol[i][3], marker='o', linestyle='None', color='red')
+        plt.plot(r1_sol[i][0], r1_sol[i][1], marker='.', linestyle='None', color='blue', alpha=i/N)
+        plt.plot(r1_sol[i][2], r1_sol[i][3], marker='.', linestyle='None', color='red', alpha=i/N)
+    plt.show()
+
+    plt.plot(np.arange(0, N, 1), np.transpose(r1_sol)[0], color='blue')
+    plt.plot(np.arange(0, N, 1), np.transpose(r1_sol)[2], color='red')
     plt.show()
